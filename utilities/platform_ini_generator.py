02c00cf4883886ac7e2ce9131b350ae4f6bc55ed
Eloston <Eloston@users.noreply.github.com>
2017-05-22 14:36:36 -0700

    Switch license from GPLv3 to BSD-3-clause

--- utilities/platform_ini_generator.py
+++ utilities/platform_ini_generator.py
@@ -1,22 +1,8 @@
 #!/usr/bin/env python3
 
-# ungoogled-chromium-binaries: Concerns prebuilt versions of ungoogled-chromium
-# Copyright (C) 2016  ungoogled-software contributors
-#
-# This file is part of ungoogled-chromium-binaries.
-#
-# ungoogled-chromium-binaries is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# ungoogled-chromium-binaries is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with ungoogled-chromium-binaries.  If not, see <http://www.gnu.org/licenses/>.
+# Copyright (c) 2017 The ungoogled-chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
 
 '''
 This script takes in files and generates a platform ini as if they were uploaded to the Releases page
