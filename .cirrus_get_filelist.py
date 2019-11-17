605b775087205b2a3d3c1dda77849fada6ecb5bf
Eloston <eloston@programmer.net>
2019-11-17 02:13:19 +0000

    Cirrus CI: Add script to get filelist
    In PRs, the previous command may not find the base SHA-1 to use. This
    script will try different variables to get a valid commit range to
    check.

---
+++ .cirrus_get_filelist.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import subprocess
+
+
+def main():
+    # Get current SHA
+    head_sha = os.environ['CIRRUS_CHANGE_IN_REPO']
+
+    # Get base SHA
+    # Set default to one commit before
+    base_sha = head_sha + '^'
+    for candidate_env in ('CIRRUS_LAST_GREEN_CHANGE', 'CIRRUS_BASE_SHA'):
+        candidate_sha = os.getenv(candidate_env)
+        if candidate_sha:
+            base_sha = candidate_sha
+            break
+
+    # Get file list via git
+    print(
+        subprocess.run(('git', 'diff', '--name-only', f'{base_sha}..{head_sha}'),
+                       check=True,
+                       capture_output=True,
+                       text=True).stdout().strip())
+
+
+if __name__ == '__main__':
+    main()
